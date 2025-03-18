f504f3b69b18764423a0d55d646cf8930c2f3678
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-03-18 16:58:16 +0100

    starter of project-section

--- src/components/StoryblokProvider.tsx
+++ src/components/StoryblokProvider.tsx
@@ -4,12 +4,14 @@ import { PropsWithChildren } from "react";
 import page from "./page";
 import { Hero } from "./storyblok/hero";
 import { Links } from "./storyblok/links";
+import { ProjectBlock } from "./storyblok/project-block";
 
 storyblokInit({
   components: {
     page: page,
     hero: Hero,
     links: Links,
+    project_block: ProjectBlock,
   },
 
   enableFallbackComponent: true,
